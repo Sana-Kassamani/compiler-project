07e32e6541784453b25083583264fc93b622be9b
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-11-23 23:23:34 +0200

    CSS <> Style - fix component's style

--- frontend/src/components/CodeEditorWindow.jsx
+++ frontend/src/components/CodeEditorWindow.jsx
@@ -1,43 +1,48 @@
 import React, { useRef, useState } from "react";
 import Editor from "@monaco-editor/react";
 import LanguageSelector from "./LanguageSelector";
+import '../styles/editor.css';
 
 const CodeEditorWindow = () => {
     const [value, setValue] = useState("");
     const editorRef = useRef();
     const [language, setLanguage] = useState("javascript");
 
-    // handle value
     const handleEditorChange = (value) => {
         setValue(value);
-    }
+    };
 
-    // handle focus on the editor
     const onMount = (editor) => {
         editorRef.current = editor;
         editor.focus();
-    }
+    };
 
     const onSelect = (language) => {
         setLanguage(language);
-      };
+    };
 
     return (
-        <div>
-            <LanguageSelector language={language} onSelect={onSelect}/>
-            <Editor 
-                height="70vh"
-                width={`100%`}
-                defaultLanguage="javascript"
-                language={language}
-                value={value}
-                theme="vs-dark"
-                defaultValue="// some comment"
-                onMount={onMount}
-                onChange={handleEditorChange}
-            />
+        <div className="CodeEditorWindow">
+            <div className="language-selector">
+                <LanguageSelector language={language} onSelect={onSelect} />
+            </div>
+            <div className="editor-container">
+                <div className="monaco-editor-container">
+                    <Editor 
+                        height="70vh"
+                        width="100%"
+                        defaultLanguage="javascript"
+                        language={language}
+                        value={value}
+                        theme="vs-dark"
+                        defaultValue="// some comment"
+                        onMount={onMount}
+                        onChange={handleEditorChange}
+                    />
+                </div>
+            </div>
         </div>
-    )
+    );
 };
 
 export default CodeEditorWindow;
