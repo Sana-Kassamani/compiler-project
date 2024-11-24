440fe6d7abae79bae4e5a371000830d8d98751dc
ArefAH <arefabouhamdan@gmail.com>
2024-11-24 02:20:57 +0200

    Laravel <> Edit - sendMail function with receiver email and invitation email

--- backend/app/Http/Controllers/MailController.php
+++ backend/app/Http/Controllers/MailController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\Invitation;
 
 class MailController extends Controller
 {
-    //
+    public function sendMail() {
+        Mail::to('arefahrm@gmail.com')->send(new Invitation());
+    }
 }
