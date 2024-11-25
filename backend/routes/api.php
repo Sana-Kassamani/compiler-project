44d81f5037cf7212cf2300ce9b4f7a0b66d3a24d
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-25 02:44:36 +0200

    Laravel <> Set - api routes for basic login/signup

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,17 @@
 <?php
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\JWTAuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::post('register', [JWTAuthController::class, 'register']);
+Route::post('login', [JWTAuthController::class, 'login']);
+Route::get('login', [JWTAuthController::class, 'unauthorized'])->name("login");
+
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('user', [JWTAuthController::class, 'getUser']);
+    Route::post('logout', [JWTAuthController::class, 'logout']);
+});
+
+
