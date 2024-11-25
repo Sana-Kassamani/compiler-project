9055ef65679d0710428b8bc1d3f4b845d975f7fc
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-25 04:57:02 +0200

    Laravel <> edit - create file function to validate for non empty paramaters

--- backend/app/Http/Controllers/Files/FilesController.php
+++ backend/app/Http/Controllers/Files/FilesController.php
@@ -11,6 +11,17 @@ use App\Models\File;
 
 class FilesController extends Controller
 {
+    public function validate($request)
+    {
+        foreach($request as $key => $value)
+        {
+            if (empty($value)){
+                return false;
+            }
+        }
+        return true;
+
+    }
     private function create_storage_file($file){
         if(!Storage::exists("CodeFiles")){
             Storage::makeDirectory("CodeFiles");
@@ -20,15 +31,23 @@ class FilesController extends Controller
     }
     public function create_file(Request $request)
     {
+        
         $user = auth()->user();
         $file_param=[
             "filename"=> $request->filename,
             "language"=> $request->language,
             "owner_id"=> $user->id, 
+            "path"=>$request->file
             ];
+        if(!$this->validate($file_param))
+        {
+            return response()->json([
+                "message"=> "All fields are required"
+            ],400);
+        }
         $path= $this->create_storage_file($request->file);
         $file_param["path"]=$path;
-        $new_file=File::create($file);
+        $new_file=File::create($file_param);
         return response()->json([
             "message"=>"File created successfully",
             "file"=> $new_file
