96a578f508a1414e02e254b85ad1a43f2b80c9da
ArefAH <arefabouhamdan@gmail.com>
2024-11-26 22:33:23 +0200

    React <> Add - add files button

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,30 +1,35 @@
 import React from "react";
-import File from '../components/File';
-import Python from '../assets/Python.png'
+import File from "../components/File";
+import Plus from "../assets/plus.svg";
+import Python from "../assets/Python.png";
 import "../styles/SideBar.css";
-
 
 const SideBar = () => {
   return (
     <div className="side-bar">
       <div className="files">
-        <h2>Files</h2>
+        <div className="files-title">
+          <h2>Files</h2>
+          <button>
+            <img src={Plus} alt="Add File" />
+          </button>
+        </div>
         <div className="file-list">
-          <File type={Python} name={'App.py'} shared={true}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'} shared={true}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'} shared={true}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'} shared={true}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'} shared={true}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'}/>
-          <File type={Python} name={'App.py'} shared={true}/>
+          <File type={Python} name={"App.py"} shared={true} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} shared={true} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} shared={true} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} shared={true} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} shared={true} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} />
+          <File type={Python} name={"App.py"} shared={true} />
         </div>
       </div>
       <div className="contributors">
@@ -34,7 +39,6 @@ const SideBar = () => {
           <h2>S</h2>
           <h2>F</h2>
           <h2>A</h2>
-          
         </div>
       </div>
     </div>
