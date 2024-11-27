04a64a75880fb4f598fc500f57acea2f7a84bc0f
ArefAH <arefabouhamdan@gmail.com>
2024-11-27 20:42:47 +0200

    Laravel <> Add - added debug and analyze methods

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,12 +5,15 @@ use App\Http\Controllers\Auth\JWTAuthController;
 use App\Http\Controllers\Files\FilesController;
 use App\Http\Middleware\JWTMiddleware;
 use App\Http\Controllers\EmailController;
+use App\Http\Controllers\AiController;
 
 Route::post('register', [JWTAuthController::class, 'register']);
 Route::post('login', [JWTAuthController::class, 'login']);
 Route::get('unauthorized', [JWTAuthController::class, 'unauthorized'])->name("unauthorized");
 Route::post('/invite', [EmailController::class, 'invite']);
 Route::put('/accept', [EmailController::class, 'accept']);
+Route::post('/debug', [AiController::class, 'debug']);
+Route::post('/analyze', [AiController::class, 'analyze']);
 
 
 Route::middleware(JWTMiddleware::class)->group(function () {
