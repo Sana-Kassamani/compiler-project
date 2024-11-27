d3d789e02dd5f08a7332cef9a1948c913f58ec7a
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 21:09:53 +0200

    React <> Edit - request method to have custom header

--- frontend/src/utils/request.js
+++ frontend/src/utils/request.js
@@ -3,14 +3,19 @@ import axios from "axios";
 axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
 // axios.defaults.headers.Authorization = localStorage.token
 
-export const request = async ({ route, method = "GET", body }) => {
+export const request = async ({
+  route,
+  method = "GET",
+  body,
+  header = "application/json",
+}) => {
   try {
     const response = await axios.request({
       url: `${route}`,
       method,
       data: body,
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": header,
         Authorization: `Bearer ${localStorage.token}`,
       },
     });
@@ -18,6 +23,7 @@ export const request = async ({ route, method = "GET", body }) => {
     return { data: response.data, status: response.status };
   } catch (error) {
     console.log("======== Error =========");
+    console.log(error);
     console.log(error.response.data.message);
     console.log("======== // =========");
 
