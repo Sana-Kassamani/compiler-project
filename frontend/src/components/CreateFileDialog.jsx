fd5fa4de86cc143588959c115044292b8f079e77
fatimaalzahraahammoud <fatimaalzahraahammoud@gmail.com>
2024-11-27 23:02:06 +0200

    React <> Add - saving, testing, and adding errors to create files dialog

--- frontend/src/components/CreateFileDialog.jsx
+++ frontend/src/components/CreateFileDialog.jsx
@@ -4,10 +4,20 @@ import "../styles/dialog.css"
 
 const CreateFileDialog = ({onClose, onCreate}) => {
     const [language, setLanguage] = useState("javascript");
+    const [fileName, setFileName] = useState("");
+    const [error, setError] = useState("");
 
     const onSelect = (language) => {
       setLanguage(language);
     };
+
+    const handleCreate = () => {
+      if (!fileName.trim()) {
+        setError("File name is required!");
+        return;
+      }
+      onCreate({ fileName, language });
+    };    
 
     return(
         <dialog className="dialog" open>
@@ -16,7 +26,9 @@ const CreateFileDialog = ({onClose, onCreate}) => {
             <div className="dialog-details">
               <div className="detail-item">
                 <label>File name:</label>
-                <input type="text" name="file" placeholder="file name" required/>
+                <input type="text" name="file" placeholder="file name" required value={fileName}
+                  onChange={(e) => setFileName(e.target.value)}
+                />
               </div>
               <div className="detail-item">
                 <label>Select Language:</label>
@@ -27,8 +39,9 @@ const CreateFileDialog = ({onClose, onCreate}) => {
             </div>
             <div className="dialog-buttons">  
               <button className="close-dialog" onClick={onClose}>Close</button>
-              <button className="create-button" onClick={onCreate}>Create</button>
+              <button className="create-button" onClick={handleCreate}>Create</button>
             </div>
+            {error && <p className="error">{error}</p>}
           </div>
         </dialog>
     );
