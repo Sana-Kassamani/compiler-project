3b5f8de576dcb2837c3293935332b6741d6b41c0
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 21:10:29 +0200

    React <> Add - file context to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,19 @@ import CodeEditorWindow from "./components/CodeEditorWindow";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import "./styles/App.css";
+import FilesProvider from "./context/fileContext";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/Register" element={<Register />} />
-          <Route path="/Home" element={<CodeEditorWindow />} />
-        </Routes>
+        <FilesProvider>
+          <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/Register" element={<Register />} />
+            <Route path="/Home" element={<CodeEditorWindow />} />
+          </Routes>
+        </FilesProvider>
       </BrowserRouter>
     </div>
   );
