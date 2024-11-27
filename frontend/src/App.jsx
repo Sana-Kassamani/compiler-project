82a05d89cebbab02baf7eed557cd718bb54ad201
ArefAH <arefabouhamdan@gmail.com>
2024-11-27 13:43:25 +0200

    React <> Add - accepted page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import CodeEditorWindow from "./components/CodeEditorWindow";
 import Login from "./components/pages/Login";
 import Register from "./components/pages/Register";
 import "./styles/App.css";
+import Accepted from "./components/pages/Accepted";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/Register" element={<Register />} />
           <Route path="/Home" element={<CodeEditorWindow />} />
+          <Route path="/Accepted" element={<Accepted />} />
+
         </Routes>
       </BrowserRouter>
     </div>
---
+++ frontend/src/components/pages/Accepted.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Check from '../../assets/check.svg'
+import '../../styles/Accepted.css'
+
+const Accepted = () => {
+  return (
+    <div>
+        <img src={Check} alt="Accepted" />
+        <h1>Invitation Accepted</h1>
+    </div>
+  )
+}
+
+export default Accepted
