3eaddc260239eed3cf0bfc72d84e8cd9a1c405ea
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 16:15:07 +0200

    React <> Debug - login request

--- frontend/src/components/pages/Login.jsx
+++ frontend/src/components/pages/Login.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import "../../styles/base/base.css";
 import "../../styles/Login.css";
 import Password from "../Password";
 import { request } from "./../../utils/request";
@@ -8,48 +9,55 @@ import { requestMethods } from "../../utils/enums/requestMethods";
 
 const Login = () => {
   const [error, setError] = useState("");
-  const [form, setForm] = useState({
+  const [form, setLoginForm] = useState({
     email: "",
     password: "",
   });
   const navigate = useNavigate();
-  const handleLogin = () => {
+  const handleLogin = async () => {
+    console.log("clicked");
     if (!validateForm(form)) {
       setError("All fields are required!");
       return;
     }
     try {
-      const response = request("login", requestMethods.POST, form);
-      localStorage.setItem("token", response.token);
-      navigate("/Home");
+      const response = await request({
+        route: "login",
+        method: requestMethods.POST,
+        body: form,
+      });
+      console.log(response);
+      if (response.status === 200) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/Home");
+      } else {
+        setError(response.data.message);
+        console.log(response.data.message);
+      }
     } catch (error) {
       setError(error.response.data.message);
       console.log(error.response.data.message);
     }
   };
-  const capturePassword = (e) => {
-    setForm((prev) => {
-      return {
-        ...prev,
-        password: e.target.value,
-      };
-    });
-  };
+  useEffect(() => {
+    console.log(form);
+  }, [form]);
+
   return (
-    <div class="login">
+    <div className="login">
       <h1>Login</h1>
       <input
         type="text"
         name="email"
         placeholder="Email address"
         required="required"
-        onChange={(e) =>
-          setForm((prev) => {
+        onChange={(e) => {
+          setLoginForm((prev) => {
             return { ...prev, email: e.target.value };
-          })
-        }
+          });
+        }}
       />
-      <Password capturePassword={capturePassword} />
+      <Password setForm={setLoginForm} />
 
       <button onClick={handleLogin}>Let me in !</button>
       <p>
