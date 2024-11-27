69ca3dc8e55d727f31cbec82ec1e7590aa7359c4
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 21:11:28 +0200

    React <> Edit - file component to select file and display content

--- frontend/src/components/File.jsx
+++ frontend/src/components/File.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useContext } from "react";
 import Invite from "../assets/invite.svg";
+import { fileContext } from "../context/fileContext";
 
-const File = ({ type, name, shared }) => {
+const File = ({ type, name, shared, userType, file, index }) => {
+  const { list, createFile, setSelectedFile } = useContext(fileContext);
+
   return (
-    <div className="file">
+    <div
+      className="file"
+      onClick={() => {
+        console.log("Selected file", file);
+        setSelectedFile(index);
+        console.log("Selected file", file);
+      }}
+    >
       <div className="info">
         <img src={type} alt="" />
-        <p>{name}</p>
+        {/* <img src={f.language} alt="" /> */}
+        <p>{file.filename}</p>
       </div>
-      {shared ? <p>Shared</p> : <img src={Invite} alt="Invite Button" />}
+      {file.shared ? (
+        <p>{file.type}</p>
+      ) : (
+        <img src={Invite} alt="Invite Button" />
+      )}
     </div>
   );
 };
