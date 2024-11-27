6c50447b0d34ae5465c4c34d810450d9d7c4d29f
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 16:15:19 +0200

    React <> Debug - signup request

--- frontend/src/components/pages/Register.jsx
+++ frontend/src/components/pages/Register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Password from "../Password";
+import "../../styles/base/base.css";
 import { useNavigate } from "react-router-dom";
 import { request } from "./../../utils/request";
 import { validateForm } from "../../utils/validate";
@@ -7,72 +8,74 @@ import { requestMethods } from "../../utils/enums/requestMethods";
 
 const Register = () => {
   const [error, setError] = useState("");
-  const [form, setForm] = useState({
+  const [form, setRegisterForm] = useState({
     username: "",
     email: "",
     password: "",
   });
   const navigate = useNavigate();
-  const handleRegister = () => {
+  const handleRegister = async () => {
     if (!validateForm(form)) {
       setError("All fields are required!");
       return;
     }
     try {
-      const response = request("register", requestMethods.POST, form);
-      localStorage.setItem("token", response.token);
-      navigate("/Home");
+      const response = await request({
+        route: "register",
+        method: requestMethods.POST,
+        body: form,
+      });
+      if (response.status === 201) {
+        localStorage.setItem("token", response.data.token);
+        navigate("/Home");
+      } else {
+        setError(response.data.message);
+      }
     } catch (error) {
       setError(error.response.data.message);
       console.log(error.response.data.message);
     }
   };
-  const capturePassword = (e) => {
-    setForm((prev) => {
-      return {
-        ...prev,
-        password: e.target.value,
-      };
-    });
-    return (
-      <div class="login">
-        <h1>Register</h1>
-        <input
-          type="text"
-          name="username"
-          placeholder="Username"
-          required="required"
-          onChange={(e) =>
-            setForm((prev) => {
-              return { ...prev, username: e.target.value };
-            })
-          }
-        />
-        <input
-          type="email"
-          name="email"
-          placeholder="Email Address"
-          required="required"
-          onChange={(e) =>
-            setForm((prev) => {
-              return { ...prev, email: e.target.value };
-            })
-          }
-        />
-        <Password capturePassword={capturePassword} />
-        <button onClick={handleRegister}>Register</button>
-        <p>
-          Already have an account?{" "}
-          <b
-            onClick={() => {
-              navigate("/");
-            }}
-          >
-            Login
-          </b>
-        </p>
-      </div>
-    );
-  };
+
+  return (
+    <div className="login">
+      <h1>Register</h1>
+      <input
+        type="text"
+        name="username"
+        placeholder="Username"
+        required="required"
+        onChange={(e) => {
+          setRegisterForm((prev) => {
+            return { ...prev, username: e.target.value };
+          });
+        }}
+      />
+      <input
+        type="email"
+        name="email"
+        placeholder="Email Address"
+        required="required"
+        onChange={(e) => {
+          setRegisterForm((prev) => {
+            return { ...prev, email: e.target.value };
+          });
+        }}
+      />
+      <Password setForm={setRegisterForm} />
+      <button onClick={handleRegister}>Register</button>
+      <p>
+        Already have an account?{" "}
+        <b
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          Login
+        </b>
+      </p>
+      {error && <p className="error-message">{error}</p>}
+    </div>
+  );
 };
 export default Register;
