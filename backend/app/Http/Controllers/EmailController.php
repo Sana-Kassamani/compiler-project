0a447ea9e2be1932972b682b76f3895828f22eed
ArefAH <arefabouhamdan@gmail.com>
2024-11-27 17:45:34 +0200

    Laravel <> Edit - added invite method in email controller

--- backend/app/Http/Controllers/EmailController.php
+++ backend/app/Http/Controllers/EmailController.php
@@ -6,8 +6,21 @@ use Illuminate\Http\Request;
 
 class EmailController extends Controller
 {
+    public function invite(Request $request){
+        $email = Invitations::create([
+            "inviting_user_id" => $request->inviting,
+            "invited_user_id" => $request->invited,
+            "file_id" => $request->file,
+            "status" => $request->status,
+            "invited_user_type" => $request->type
+        ]);
+
+        return response()->json([
+            "new_invitation" => $email
+        ]);
+    }
     public function accept($id, Request $request){
-        $course = Course::find($id)->update([
+        $email = Invitations::find($id)->update([
             
         ]);
     }
