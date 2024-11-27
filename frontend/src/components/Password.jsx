a099bc2d9dbd6c25f6d1b281918f0c520a21f4b3
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-27 05:39:34 +0200

    React <> Edit - password component to capture value from input

--- frontend/src/components/Password.jsx
+++ frontend/src/components/Password.jsx
@@ -1,23 +1,26 @@
-import React, { useState } from 'react'
-import Eye from '../assets/eye.svg'
-import ClosedEye from '../assets/eye-closed.svg'
+import React, { useState } from "react";
+import Eye from "../assets/eye.svg";
+import ClosedEye from "../assets/eye-closed.svg";
 
-const Password = () => {
-    const [type, setType] = useState('password');
-    const handleClick = () => {
-        setType((prev) => (prev === 'password' ? 'text' : 'password'));
-    };
+const Password = (capturePassword) => {
+  const [type, setType] = useState("password");
+  const handleClick = () => {
+    setType((prev) => (prev === "password" ? "text" : "password"));
+  };
   return (
     <div className="password">
-          <input
-            type={type}
-            name="password"
-            placeholder="Password"
-            required="required"
-          />
-          <div className="eye" onClick={handleClick}><img src={type === 'password' ? ClosedEye : Eye} alt="" /></div>
+      <input
+        type={type}
+        name="password"
+        placeholder="Password"
+        required="required"
+        onChange={capturePassword}
+      />
+      <div className="eye" onClick={handleClick}>
+        <img src={type === "password" ? ClosedEye : Eye} alt="" />
       </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Password
+export default Password;
